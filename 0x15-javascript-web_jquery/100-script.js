6aec1e63be7b64b25cd87d36c5117e36853c30cf
Baajike <baajikeimmanuella@gmail.com>
2024-05-02 19:08:07 -0700

    document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,9 @@
+// Wait for the DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the <header> element
+    var header = document.querySelector('header');
+    
+    // Update the text color of the <header> element to red
+    header.style.color = '#FF0000';
+});
+
