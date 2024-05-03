99b76935246d32263e2dd7f64b94ebd4982ec766
Baajike <baajikeimmanuella@gmail.com>
2024-05-02 19:01:25 -0700

    change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+// Use the JQuery API to add a click event listener to the div with id 'update_header'
+$('#update_header').click(function() {
+    // Use the JQuery API to select the <header> element and update its text
+    $('header').text('New Header!!!');
+});
+
