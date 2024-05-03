8ccd5094dc9e3174856ae802e6f6a9ceadee10d9
Baajike <baajikeimmanuella@gmail.com>
2024-05-02 18:56:34 -0700

    add red class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+// Use the JQuery API to add a click event listener to the div with id 'red_header'
+$('#red_header').click(function() {
+    // Inside the click event handler, add the class 'red' to the <header> element
+    $('header').addClass('red');
+});
+
